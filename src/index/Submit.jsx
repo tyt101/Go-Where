68223825cbf6e94508f128f48f758e9c1f6507c5
tangytin <tangytin@163.com>
2022-12-20 15:26:02 +0800

    feat: 提交按钮

---
+++ src/index/Submit.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Submit.css'
+export default function Submit() {
+  return (
+    <div className="submit">
+      <button type='submit' className='submit-button'>搜索</button>
+    </div>
+  )
+}
